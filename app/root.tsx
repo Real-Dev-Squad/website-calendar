8687304122f86d485b20f920931e8adf1fc6935c
Harshith Venkatesh <harshithece7016@gmail.com>
2022-02-20 00:28:28 +0530

    feat: integration of tailwind css with remix app

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,12 @@ import {
   ScrollRestoration
 } from "remix";
 import type { MetaFunction } from "remix";
+import styles from "./tailwind.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
