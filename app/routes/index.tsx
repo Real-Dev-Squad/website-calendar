03152bf4fd3e42190442a7fceba8f20521093166
Harshith Venkatesh <harshithece7016@gmail.com>
2022-02-26 02:32:27 +0530

    feat: global css setup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,17 @@
+import Confetti from 'react-confetti'
 export default function Index() {
+  const width = 1000;
+  const height = 1000;
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container">
+      <h4 className="border border-blue m-auto text-center text-3xl text-blue-700">
+         RDS Calendar
+        </h4>
+        <div className="flex justify-center align-middle m-auto">
+        <Confetti width={width} height={height} />
+        ⚙️Site in Progress⚙️
+        </div>
+       
     </div>
   );
 }
