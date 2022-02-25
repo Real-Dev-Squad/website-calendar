99dac1d2aa8e4bda0a57c19f065637ef34c3a3ed
Harshith Venkatesh <harshithece7016@gmail.com>
2022-02-26 00:21:56 +0530

    feat: color setup of RDS for calender site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
+    color: {
+      resolutionBlue:	'#041484',
+      razzmatazz: '#E30464',
+      feijoa: '#96D882',
+      fern:	'#6DB470'
+    },
     extend: {}
   },
   variants: {},
