b7d534b2c271faf889ac35bd2362515c5d78b064
harshith-powerviz <harshith@powerviz.ai>
2022-08-11 23:58:03 +0530

    Calendar client view changes

--- app/components/Calendar.client.tsx
+++ app/components/Calendar.client.tsx
@@ -1,9 +1,19 @@
 import ToastUICalendar from '@toast-ui/react-calendar';
+import { useState } from 'react';
 
-const Calendar = () => (
-  <section>
-    <ToastUICalendar height="100vh" />
-  </section>
-);
+const Calendar = () => {
+  const calendarViews = ["month","week","day"]
+  const [calendarView, setCalendarView] = useState(calendarViews[0]);
+  return (
+  <>
+    <h4 className="border border-blue m-auto text-center text-3xl text-blue-700">RDS Calendar</h4>
+    <div>
+    {calendarViews.map((calendarViewData: string,index:number) => (
+      <button key={index} onClick={() => setCalendarView(calendarViewData)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5">{calendarViewData}</button>
+    ))}
+    </div>
+    <ToastUICalendar view={calendarView as any} height="100vh" />
+  </>
+)};
 
 export default Calendar;
