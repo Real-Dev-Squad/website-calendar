3b49f5eae8ad1a5af21dd78191fd51e60384a1f5
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-11-05 14:13:42 +0530

    Add action to pill onClick

--- app/components/Pill.tsx
+++ app/components/Pill.tsx
@@ -1,9 +1,12 @@
+import { MouseEventHandler } from 'react';
+
 interface PillProps {
   title: string;
   icon: string;
+  action: MouseEventHandler;
 }
-const Pill: React.FC<PillProps> = ({ title, icon = '💖,#️⃣' }) => (
-  <button className="relative rounded-full bg-slate-200 group py-2 pr-4 pl-3">
+const Pill: React.FC<PillProps> = ({ title, icon = '💖,#️⃣', action }) => (
+  <button className="relative rounded-full bg-slate-200 group py-2 pr-4 pl-3" onClick={action}>
     <i className="absolute pr-2 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
       {icon.split(',')[0]}
     </i>
