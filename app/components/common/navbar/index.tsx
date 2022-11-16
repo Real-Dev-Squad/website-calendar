aaa69e99ef430b6ba3b754319693c055987abf2a
vivek lokhande <vivek2811999@gmail.com>
2022-11-16 10:45:35 +0530

    feat(navbar):added navbar code with tailwind

--- app/components/common/navbar/index.tsx
+++ app/components/common/navbar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   HomeIcon,
   CalendarIcon,
@@ -7,48 +7,103 @@ import {
   QuestionMarkCircleIcon,
 } from '@heroicons/react/24/outline';
 
-const Navbar = () => (
-  <nav className="w-52 flex flex-col h-screen justify-between bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 navbar">
-    <div>
-      <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100  md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li className="flex items-center mb-8">
-          <div className="h-5 w-5  ">
-            <HomeIcon />
-          </div>
-          <p className="pl-2  text-base text fern">Home</p>
-        </li>
-        <li className="flex items-center mb-8">
-          <div className="h-5 w-5  ">
-            <CalendarIcon />
-          </div>
-          <p className="pl-2  text-base">Events</p>
-        </li>
-        <li className="flex items-center mb-8">
-          <div className="h-5 w-5  ">
-            <UsersIcon />
-          </div>
-          <p className="pl-2  text-base">Social Events</p>
-        </li>
-      </ul>
-    </div>
+const Navbar = () => {
+  const [active, setActive] = useState<number>(0);
 
-    <div>
-      <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100  md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li className="flex items-center mb-8">
-          <div className="h-5 w-5  ">
-            <Cog6ToothIcon />
-          </div>
-          <p className="pl-2  text-base">Settings</p>
-        </li>
-        <li className="flex items-center ">
-          <div className="h-5 w-5  ">
-            <QuestionMarkCircleIcon />
-          </div>
-          <p className="pl-2  text-base">Help</p>
-        </li>
-      </ul>
-    </div>
-  </nav>
-);
+  const myColor = (elementPosition: number) => {
+    if (active === elementPosition) return true;
+    return false;
+  };
+  const toggle = (elementPosition: number) => {
+    if (active !== elementPosition) setActive(elementPosition);
+  };
+  return (
+    <nav className="w-52 flex flex-col h-screen justify-between  px-2 sm:px-4 py-2.5 border-solid border-2 border-stone-200 bg-stone-50">
+      <div>
+        <ul className="flex flex-col p-2 mt-4 md:mt-0 md:text-sm md:font-medium bg-stone-50">
+          <li className="flex items-center mb-8 cursor-pointer group" onClick={() => toggle(0)}>
+            <div className="h-5 w-5">
+              <HomeIcon
+                className={`${
+                  myColor(0) ? ' stroke-slate-900' : ' stroke-stone-500'
+                } group-hover:stroke-slate-900`}
+              />
+            </div>
+            <p
+              className={`pl-2  text-base  hover:text-slate-900 ${
+                myColor(0) ? ' text-slate-900' : ' text-stone-500'
+              }`}
+            >
+              Home
+            </p>
+          </li>
+          <li className="flex items-center mb-8 cursor-pointer  group" onClick={() => toggle(1)}>
+            <div className="h-5 w-5 group-hover:stroke-slate-900 ">
+              <CalendarIcon
+                className={`${
+                  myColor(1) ? ' stroke-slate-900' : ' stroke-stone-500'
+                }  group-hover:stroke-slate-900`}
+              />
+            </div>
+            <p
+              className={`pl-2 text-base hover:text-slate-900 ${
+                myColor(1) ? ' text-slate-900' : ' text-stone-500'
+              }`}
+            >
+              Events
+            </p>
+          </li>
+          <li className="flex items-center mb-8 cursor-pointer  group" onClick={() => toggle(2)}>
+            <div className="h-5 w-5 group-hover:stroke-slate-900 ">
+              <UsersIcon
+                className={`${
+                  myColor(2) ? ' stroke-slate-900' : ' stroke-stone-500'
+                }  group-hover:stroke-slate-900`}
+              />
+            </div>
+            <p
+              className={`pl-2 text-base hover:text-slate-900 ${
+                myColor(2) ? ' text-slate-900' : ' text-stone-500'
+              }`}
+            >
+              Social Events
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <ul className="flex flex-col p-2 mt-4 md:mt-0 md:text-sm md:font-medium md:border-0 bg-stone-50">
+          <li className="flex items-center mb-8 cursor-pointer group" onClick={() => toggle(3)}>
+            <div className="h-5 w-5  group">
+              <Cog6ToothIcon
+                className={`${
+                  myColor(3) ? ' stroke-slate-900' : ' stroke-stone-500'
+                }  group-hover:stroke-slate-900`}
+              />
+            </div>
+            <p
+              className={`pl-2 text-base hover:text-slate-900 ${
+                myColor(3) ? ' text-slate-900' : ' text-stone-500'
+              }`}
+            >
+              Settings
+            </p>
+          </li>
+          <li className="flex items-center cursor-pointer  group" onClick={() => toggle(4)}>
+            <div className="h-5 w-5">
+              <QuestionMarkCircleIcon
+                className={`${
+                  myColor(4) ? ' stroke-slate-900' : ' stroke-stone-500'
+                } group-hover:stroke-slate-900`}
+              />
+            </div>
+            <p className="pl-2 text-base text-stone-500 hover:stroke-slate-900">Help</p>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,17 @@
+import Navbar from '~/components/common/navbar';
+
 export default function Index() {
   return (
-    <div className="container">
-      <h4 className="m-auto text-3xl text-center text-blue-700 border border-blue">RDS Calendar</h4>
-      <div className="flex justify-center m-auto align-middle">⚙️Site in Progress⚙️</div>
+    <div className="">
+      <div className="flex">
+        <Navbar />
+        <div>
+          <h4 className="m-auto text-3xl text-center text-blue-700 border border-blue">
+            RDS Calendar
+          </h4>
+          <div className="flex justify-center m-auto align-middle">⚙️Site in Progress⚙️</div>
+        </div>
+      </div>
     </div>
   );
 }
