dcdc1ae69ac7f5aef84472da9134b36f34e624a5
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-11-17 18:47:51 +0530

    add passing icons as prop

--- app/components/Pill.client.tsx
+++ app/components/Pill.client.tsx
@@ -1,17 +1,26 @@
-import React, { MouseEventHandler } from 'react';
+import React, { ComponentType, MouseEventHandler } from 'react';
+import { HashtagIcon, HeartIcon } from '@heroicons/react/24/solid';
+import { IconProps } from '~/types/Icon';
 
 interface PillProps {
   title: string;
-  icon?: string;
+  IconA?: ComponentType<IconProps>;
+  IconB?: ComponentType<IconProps>;
   action?: MouseEventHandler;
 }
-const Pill: React.FC<PillProps> = ({ title, icon = '💖,#️⃣', action }) => (
+
+const Pill: React.FC<PillProps> = ({
+  title,
+  IconA = HeartIcon,
+  IconB = HashtagIcon,
+  action,
+}: PillProps) => (
   <button className="relative rounded-full bg-slate-200 group py-2 pr-4 pl-3" onClick={action}>
     <span className="absolute pr-2 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
-      {icon.split(',')[0]}
+      <IconA className="h-6 w-6 text-red-500" />
     </span>
     <span className="absolute pr-2 opacity-100 group-hover:opacity-0 ease-in-out duration-300">
-      {icon.split(',')[1]}
+      <IconB className="h-6 w-6 text-blue-500" />
     </span>
     <span className="pl-6">{title}</span>
   </button>
