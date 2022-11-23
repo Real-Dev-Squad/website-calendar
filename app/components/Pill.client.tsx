eda93a86bfcaab441ee9469474f561bb21a5779b
Ritik Jaiswal <ritik12103032000@gmail.com>
2022-11-23 09:18:33 +0530

    add test and generic names

--- app/components/Pill.client.tsx
+++ app/components/Pill.client.tsx
@@ -4,23 +4,23 @@ import { IconProps } from '~/types/Icon';
 
 interface PillProps {
   title: string;
-  IconA?: ComponentType<IconProps>;
-  IconB?: ComponentType<IconProps>;
+  Icon?: ComponentType<IconProps>;
+  IconOnHover?: ComponentType<IconProps>;
   action?: MouseEventHandler;
 }
 
 const Pill: React.FC<PillProps> = ({
   title,
-  IconA = HeartIcon,
-  IconB = HashtagIcon,
+  Icon = HashtagIcon,
+  IconOnHover = HeartIcon,
   action,
 }: PillProps) => (
   <button className="relative rounded-full bg-slate-200 group py-2 pr-4 pl-3" onClick={action}>
-    <span className="absolute pr-2 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
-      <IconA className="h-6 w-6 text-red-500" />
-    </span>
     <span className="absolute pr-2 opacity-100 group-hover:opacity-0 ease-in-out duration-300">
-      <IconB className="h-6 w-6 text-blue-500" />
+      <Icon data-testid="icon" className="h-6 w-6 text-blue-500" />
+    </span>
+    <span className="absolute pr-2 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
+      <IconOnHover data-testid="icon-on-hover" className="h-6 w-6 text-red-500" />
     </span>
     <span className="pl-6">{title}</span>
   </button>
