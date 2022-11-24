013b76ccd3f1bb6902e744035b53b06cfa1a5c15
vivek lokhande <vivek2811999@gmail.com>
2022-11-24 17:19:13 +0530

    feat(navbar): added class shorthands

--- app/components/common/navbar/index.tsx
+++ app/components/common/navbar/index.tsx
@@ -84,28 +84,47 @@ const Navbar = () => {
     ],
   };
 
+  const classShortHands = {
+    flexDesktopCol: 'flex flex-row md:flex-col',
+    flexDesktopRow: 'flex flex-col md:flex-row',
+    fontSize: 'text-xs md:text-base',
+    padding: 'px-0 md:px-2',
+    headingSelectedColor: 'text-slate-900',
+    headingColor: 'text-stone-500',
+  };
+
   return (
-    <nav className="w-full md:w-52 flex flex-row md:flex-col h-auto md:h-screen justify-between  px-0 md:px-2 sm:px-4 py-2.5 border-t-2  md:border-r-2  border-stone-200 bg-stone-50 fixed bottom-0 md:static">
+    <nav
+      className={`w-full md:w-52 ${classShortHands.flexDesktopCol} md:h-screen justify-between  ${classShortHands.padding} sm:px-4 border-t-2  md:border-r-2  border-stone-200 bg-stone-50 fixed bottom-0 md:static`}
+    >
       <div className="basis-full">
-        <ul className="flex flex-row md:flex-col  justify-evenly px-0 md:px-2  mt-0 md:mt-4 md:text-sm md:font-medium bg-stone-50">
+        <ul
+          className={`${classShortHands.flexDesktopCol}   justify-evenly ${classShortHands.padding} py-2  mt-0 md:mt-7 md:text-sm md:font-medium bg-stone-50`}
+        >
           {navbarElements.arr1.map((navIntem: NavItemType) => (
             <li
-              className={`${
-                navIntem.visibleOnDesktop ? '' : 'md:hidden'
-              } flex flex-col md:flex-row items-center mb-0 md:mb-8 cursor-pointer group`}
+              className={`${navIntem.visibleOnDesktop ? '' : 'md:hidden'} ${
+                classShortHands.flexDesktopRow
+              } items-center mb-0 md:mb-8 cursor-pointer group`}
               onClick={() => toggle(navIntem.id)}
             >
               <div className="h-5 w-5">
                 <DynamicHeroIcon
                   name={navIntem.icon}
                   className={`${
-                    myColor(navIntem.id) ? ' stroke-slate-900' : ' stroke-stone-500'
-                  } group-hover:stroke-slate-900`}
+                    myColor(navIntem.id)
+                      ? `${classShortHands.headingSelectedColor}`
+                      : `${classShortHands.headingColor}`
+                  } group-hover:${classShortHands.headingSelectedColor}`}
                 />
               </div>
               <p
-                className={`p-0 md:p-0 md:pl-2   text-xs md:text-base   hover:text-slate-900 ${
-                  myColor(navIntem.id) ? ' text-slate-900' : ' text-stone-500'
+                className={`p-0 md:p-0 md:pl-2   ${
+                  classShortHands.fontSize
+                }  hover:text-slate-900 ${
+                  myColor(navIntem.id)
+                    ? `${classShortHands.headingSelectedColor}`
+                    : `${classShortHands.headingColor}`
                 }`}
               >
                 {navIntem.text}
@@ -116,25 +135,31 @@ const Navbar = () => {
       </div>
 
       <div>
-        <ul className="hidden md:flex flex-col p-2 mt-4 md:mt-0 md:text-sm md:font-medium md:border-0 bg-stone-50">
+        <ul
+          className={`hidden md:flex flex-col ${classShortHands.padding} mt-4 md:mt-0 md:text-sm md:font-medium md:border-0 bg-stone-50`}
+        >
           {navbarElements.arr2.map((navIntem: NavItemType) => (
             <li
-              className={`${
-                navIntem.visibleOnDesktop ? '' : 'md:hidden'
-              } flex flex-col md:flex-row items-center mb-0 md:mb-8 cursor-pointer group`}
+              className={`${navIntem.visibleOnDesktop ? '' : 'md:hidden'}  ${
+                classShortHands.flexDesktopRow
+              } items-center mb-0 md:mb-8 cursor-pointer group`}
               onClick={() => toggle(navIntem.id)}
             >
               <div className="h-5 w-5">
                 <DynamicHeroIcon
                   name={navIntem.icon}
                   className={`${
-                    myColor(navIntem.id) ? ' stroke-slate-900' : ' stroke-stone-500'
+                    myColor(navIntem.id)
+                      ? `$${classShortHands.headingSelectedColor}`
+                      : `${classShortHands.headingColor}`
                   } group-hover:stroke-slate-900`}
                 />
               </div>
               <p
-                className={`p-0 md:pl-2   text-xs md:text-base   hover:text-slate-900 ${
-                  myColor(navIntem.id) ? ' text-slate-900' : ' text-stone-500'
+                className={`p-0 md:pl-2   ${classShortHands.fontSize}   hover:text-slate-900 ${
+                  myColor(navIntem.id)
+                    ? `${classShortHands.headingSelectedColor}`
+                    : `${classShortHands.headingColor}`
                 }`}
               >
                 {navIntem.text}
