c6329177dc123049e869cefb606e1d3a902273fe
Ritik Jaiswal <ritik12103032000@gmail.com>
2022-11-25 09:16:58 +0530

    take classnames as props

--- app/components/Pill.client.tsx
+++ app/components/Pill.client.tsx
@@ -7,6 +7,9 @@ interface PillProps {
   Icon?: ComponentType<IconProps>;
   IconOnHover?: ComponentType<IconProps>;
   action?: MouseEventHandler;
+  pillBackground?: string;
+  iconColor?: string;
+  iconHoverColor?: string;
 }
 
 const Pill: React.FC<PillProps> = ({
@@ -14,13 +17,19 @@ const Pill: React.FC<PillProps> = ({
   Icon = HashtagIcon,
   IconOnHover = HeartIcon,
   action,
+  pillBackground = 'bg-slate-200',
+  iconColor = 'text-blue-500',
+  iconHoverColor = 'text-red-500',
 }: PillProps) => (
-  <button className="relative rounded-full bg-slate-200 group py-2 pr-4 pl-3" onClick={action}>
+  <button
+    className={`relative rounded-full group py-2 pr-4 pl-3 ${pillBackground}`}
+    onClick={action}
+  >
     <span className="absolute pr-2 opacity-100 group-hover:opacity-0 ease-in-out duration-300">
-      <Icon data-testid="icon" className="h-6 w-6 text-blue-500" />
+      <Icon data-testid="icon" className={`h-6 w-6 ${iconColor}`} />
     </span>
     <span className="absolute pr-2 opacity-0 group-hover:opacity-100 ease-in-out duration-300">
-      <IconOnHover data-testid="icon-on-hover" className="h-6 w-6 text-red-500" />
+      <IconOnHover data-testid="icon-on-hover" className={`h-6 w-6 ${iconHoverColor}`} />
     </span>
     <span className="pl-6">{title}</span>
   </button>
