a11e5c99edd1138ae1631404ea9514eff4e97e35
vivek lokhande <vivek2811999@gmail.com>
2022-11-28 13:13:03 +0530

    feat(onboarding):added onboarding route

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,20 +1,14 @@
 import Navbar from '~/components/common/navbar';
-import UserOnboarding from '~/components/userOnboarding';
 
 export default function Index() {
   return (
-    <div className="">
-      {/* <div className="flex flex-col-reverse md:flex-row ">
-        <Navbar />
-        <div>
-          <h4 className="m-auto text-3xl text-center text-blue-700 border border-blue">
-            RDS Calendar
-          </h4>
-          <div className="flex justify-center m-auto align-middle">⚙️Site in Progress⚙️</div>
-        </div>
-      </div> */}
+    <div className="flex flex-col-reverse md:flex-row ">
+      <Navbar />
       <div>
-        <UserOnboarding />
+        <h4 className="m-auto text-3xl text-center text-blue-700 border border-blue">
+          RDS Calendar
+        </h4>
+        <div className="flex justify-center m-auto align-middle">⚙️Site in Progress⚙️</div>
       </div>
     </div>
   );
