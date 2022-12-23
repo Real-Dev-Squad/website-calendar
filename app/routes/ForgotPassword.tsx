b2950f301be729d3694493803fefd4e16c6bf244
Bhavika Tibrewal <bhtibrewal@gmail.com>
2022-12-23 22:59:00 +0530

    change file name

--- app/routes/ForgotPassword.tsx
+++ app/routes/ForgotPassword.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'remix';
 import { Button } from '~/components/Button';
 import UserInput from '../components/common/userInput';
 const ForgotPassword = () => {
   const navigate = useNavigate();
+  const [email, setEmail] = useState('');
   return (
     <div className="flex items-center justify-center w-screen min-h-screen">
       <main className="mx-auto shadow-xl min-w-fit w-1/3 max-w-lg px-6 pt-10 pb-9 rounded-2xl">
@@ -12,7 +13,7 @@ const ForgotPassword = () => {
           No worries, we'll send you a verification code.
         </h3>
         <form className="mt-8">
-          <UserInput label="Email" placeholder="" />
+          <UserInput label="Email" placeholder="" value={email} setValue={setEmail} />
           <Button
             size="medium"
             label="Send verification code"
