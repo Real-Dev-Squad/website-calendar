1ad92f47345158a2e65adc74814c6eb768345753
Rohit Gaur <knotjinx@gmail.com>
2022-12-27 22:47:56 -0500

    fix: update file names, imports

--- app/routes/ResetPassword.tsx
+++ app/routes/ResetPassword.tsx
@@ -3,7 +3,7 @@ import { Button } from '~/components/Button';
 import UserInput from '~/components/common/userInput';
 
 const ResetPassword = () => {
-  const [state, setState] = useState({ password: '', confirm_password: '' });
+  const [state, setState] = useState({ password: '', confirmPassword: '' });
   return (
     <div className="flex items-center  w-screen min-h-screen ">
       <main className="mx-auto shadow-xl  px-6 py-10 min-w-fit w-1/3 max-w-lg rounded-2xl flex flex-col gap-4 ">
@@ -19,8 +19,8 @@ const ResetPassword = () => {
           type="password"
           label={'Confirm Password'}
           description="Both passwords should be the same."
-          value={state.confirm_password}
-          setValue={(val) => setState((prev) => ({ ...prev, confirm_password: val }))}
+          value={state.confirmPassword}
+          setValue={(val) => setState((prev) => ({ ...prev, confirmPassword: val }))}
         />
         <Button label="Reset Password" size="medium" varient="primary" />
       </main>
