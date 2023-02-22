1cbe05c9b6c111e3c92d02b7fa9b1fba2355a265
sinhayash9546 <sinhayash9546@gmail.com>
2023-02-23 01:05:51 +0530

    Resolved PR Reviews

---
+++ app/components/searchBar/index.test.tsx
@@ -0,0 +1,59 @@
+import { screen, render, fireEvent, getByTestId } from '@testing-library/react'
+import SearchBar from '.'
+
+it('renders a input field', () => {
+    render(
+        <SearchBar
+            placeholder="Search"
+            defaultValue=""
+            onChangeValue={() => { }}
+        />
+    );
+
+    const searchBar = screen.getByTestId('searchBar');
+    expect(searchBar).toBeInTheDocument();
+});
+
+
+it('checks input value to be empty', () => {
+    render(
+        <SearchBar
+            placeholder=''
+            defaultValue=""
+            onChangeValue={() => { }}
+        />
+    );
+    const inputElement = screen.getByTestId('searchBar');
+    expect(inputElement).toBeInTheDocument();
+})
+
+
+it('checks input letter', () => {
+
+    render(
+        <SearchBar
+            placeholder=''
+            onChangeValue={jest.fn((value) => { })}
+        />
+    );
+    const letters = 'abcdefghizklmnopqrstuvwxyz'
+    const input = screen.getByTestId('searchBar') as HTMLInputElement;
+    fireEvent.change(input, { target: { value: letters } })
+    expect(input).toHaveValue(letters);
+})
+
+
+it('check input symbols', () => {
+    render(
+        <SearchBar
+            placeholder=''
+            defaultValue=""
+            onChangeValue={() => { }}
+        />
+    );
+    const symbols = "!@#$%^&*()_+-="
+    const inputSymbols = screen.getByTestId('searchBar') as HTMLInputElement;
+    fireEvent.change(inputSymbols, { target: { value: symbols } })
+    expect(inputSymbols).toHaveValue(symbols);
+})
+
---
+++ app/components/searchBar/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useState} from "react";
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
+//for passing props creating interface 
+interface SearchProps {
+    placeholder?: string,
+    defaultValue?: string,
+    onChangeValue: (value: string)=>void
+}
+
+
+const SearchBar: React.FC<SearchProps> = ({ 
+placeholder = "Search",
+defaultValue,
+onChangeValue }) => {
+     
+
+    return (
+
+            <div className="flex justify-center bg-gray-100 rounded-xl  border-2 overflow-hidden w-80 m-2">
+                <MagnifyingGlassIcon className="h-8.5 w-5 mx-2 text-gray-500" />
+                <input data-testid="searchBar" type="search" placeholder={placeholder}
+                 defaultValue={defaultValue} onChange={(e)=>onChangeValue(e.target.value)} className="block rounded-md border-0 flex-grow p-2 bg-gray-100 !outline-none" />
+            </div>
+    )
+}
+
+export default SearchBar;
+
