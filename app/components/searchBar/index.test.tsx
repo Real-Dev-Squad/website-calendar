69538be577f80a851fde4b5d5b2785a420a1edb1
sinhayash9546 <sinhayash9546@gmail.com>
2023-02-23 01:18:49 +0530

    chore: format code

--- app/components/searchBar/index.test.tsx
+++ app/components/searchBar/index.test.tsx
@@ -1,59 +1,31 @@
-import { screen, render, fireEvent, getByTestId } from '@testing-library/react'
-import SearchBar from '.'
+import { screen, render, fireEvent, getByTestId } from '@testing-library/react';
+import SearchBar from '.';
 
 it('renders a input field', () => {
-    render(
-        <SearchBar
-            placeholder="Search"
-            defaultValue=""
-            onChangeValue={() => { }}
-        />
-    );
+  render(<SearchBar placeholder="Search" defaultValue="" onChangeValue={() => {}} />);
 
-    const searchBar = screen.getByTestId('searchBar');
-    expect(searchBar).toBeInTheDocument();
+  const searchBar = screen.getByTestId('searchBar');
+  expect(searchBar).toBeInTheDocument();
 });
 
-
 it('checks input value to be empty', () => {
-    render(
-        <SearchBar
-            placeholder=''
-            defaultValue=""
-            onChangeValue={() => { }}
-        />
-    );
-    const inputElement = screen.getByTestId('searchBar');
-    expect(inputElement).toBeInTheDocument();
-})
-
+  render(<SearchBar placeholder="" defaultValue="" onChangeValue={() => {}} />);
+  const inputElement = screen.getByTestId('searchBar');
+  expect(inputElement).toBeInTheDocument();
+});
 
 it('checks input letter', () => {
-
-    render(
-        <SearchBar
-            placeholder=''
-            onChangeValue={jest.fn((value) => { })}
-        />
-    );
-    const letters = 'abcdefghizklmnopqrstuvwxyz'
-    const input = screen.getByTestId('searchBar') as HTMLInputElement;
-    fireEvent.change(input, { target: { value: letters } })
-    expect(input).toHaveValue(letters);
-})
-
+  render(<SearchBar placeholder="" onChangeValue={jest.fn((value) => {})} />);
+  const letters = 'abcdefghizklmnopqrstuvwxyz';
+  const input = screen.getByTestId('searchBar') as HTMLInputElement;
+  fireEvent.change(input, { target: { value: letters } });
+  expect(input).toHaveValue(letters);
+});
 
 it('check input symbols', () => {
-    render(
-        <SearchBar
-            placeholder=''
-            defaultValue=""
-            onChangeValue={() => { }}
-        />
-    );
-    const symbols = "!@#$%^&*()_+-="
-    const inputSymbols = screen.getByTestId('searchBar') as HTMLInputElement;
-    fireEvent.change(inputSymbols, { target: { value: symbols } })
-    expect(inputSymbols).toHaveValue(symbols);
-})
-
+  render(<SearchBar placeholder="" defaultValue="" onChangeValue={() => {}} />);
+  const symbols = '!@#$%^&*()_+-=';
+  const inputSymbols = screen.getByTestId('searchBar') as HTMLInputElement;
+  fireEvent.change(inputSymbols, { target: { value: symbols } });
+  expect(inputSymbols).toHaveValue(symbols);
+});
