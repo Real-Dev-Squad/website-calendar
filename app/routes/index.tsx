856cc61611bc1024a4ad87975b6aaa064fb5da10
Rohit Gaur <knotjinx@gmail.com>
2023-04-09 11:40:54 -0400

    fix: remove styles file

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,5 @@
-import { LinksFunction } from '@remix-run/node';
-import styles from 'react-datepicker/dist/react-datepicker.css';
 import Calendar from '~/components/Calendar';
 import Navbar from '~/components/common/navbar';
-
-export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export default function Index() {
   return (
