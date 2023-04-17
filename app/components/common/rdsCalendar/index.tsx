1a7556841f851e09efd6e105b1380b0bb94a957f
Martijn Vermeulen <martijn.vermeulen10@gmail.com>
2023-04-17 13:55:42 +0200

    momentLocalizer to dayjsLocalizer

--- app/components/common/rdsCalendar/index.tsx
+++ app/components/common/rdsCalendar/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import { Calendar, DateLocalizer } from 'react-big-calendar';
 import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
 import withDragAndDrop, { withDragAndDropProps } from 'react-big-calendar/lib/addons/dragAndDrop';
 import { CalendarEventProps, CalEvent, UpdateEvent } from '~/utils/interfaces';
 
@@ -9,11 +10,70 @@ interface RdsCalendarProps {
   eventsList: CalEvent[];
   currentEvent: CalEvent | undefined;
   setCalendarEvent: React.Dispatch<React.SetStateAction<CalendarEventProps | undefined>>;
+  // eslint-disable-next-line no-unused-vars
   updateEvent: ({ event, start, end }: UpdateEvent) => void;
 }
 
-// TODO: localize with dayjs instead of moment
-const localizer = momentLocalizer(moment);
+dayjs.extend(localeData);
+
+const dateRangeFormat = ({ start, end }: any, culture: any, local: any) =>
+  `${local.format(start, 'L', culture)} - ${local.format(end, 'L', culture)}`;
+
+const timeRangeFormat = ({ start, end }: any, culture: any, local: any) =>
+  `${local.format(start, 'LT', culture)} - ${local.format(end, 'LT', culture)}`;
+
+const timeRangeStartFormat = ({ start }: any, culture: any, local: any) =>
+  `${local.format(start, 'LT', culture)} - `;
+
+const timeRangeEndFormat = ({ end }: any, culture: any, local: any) =>
+  ` - ${local.format(end, 'LT', culture)}`;
+
+const weekRangeFormat = ({ start, end }: any, culture: any, local: any) =>
+  `${local.format(start, 'MMMM DD', culture)} - ${local.format(
+    end,
+    local.eq(start, end, 'month') ? 'DD' : 'MMMM DD',
+    culture,
+  )}`;
+
+const formats = {
+  dateFormat: 'DD',
+  dayFormat: 'DD ddd',
+  weekdayFormat: 'ddd',
+
+  selectRangeFormat: timeRangeFormat,
+  eventTimeRangeFormat: timeRangeFormat,
+  eventTimeRangeStartFormat: timeRangeStartFormat,
+  eventTimeRangeEndFormat: timeRangeEndFormat,
+
+  timeGutterFormat: 'LT',
+
+  monthHeaderFormat: 'MMMM YYYY',
+  dayHeaderFormat: 'dddd MMM DD',
+  dayRangeHeaderFormat: weekRangeFormat,
+  agendaHeaderFormat: dateRangeFormat,
+
+  agendaDateFormat: 'ddd MMM DD',
+  agendaTimeFormat: 'LT',
+  agendaTimeRangeFormat: timeRangeFormat,
+};
+
+const dayjsLocalizer = () => {
+  const locale = (m: any, c: any) => (c ? m.locale(c) : m);
+
+  return new DateLocalizer({
+    formats,
+    firstOfWeek(culture) {
+      const data = dayjs.localeData();
+      return data ? data.firstDayOfWeek() : 0;
+    },
+
+    format(value, format, culture) {
+      return locale(dayjs(value), culture).format(format);
+    },
+  });
+};
+
+const localizer = dayjsLocalizer();
 
 const RdsCalendar = ({
   eventsList,
