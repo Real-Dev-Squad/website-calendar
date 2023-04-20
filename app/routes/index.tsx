5acaecfd9f026a1547c22b31f5c3dec23628a890
vivek lokhande <vivek2811999@gmail.com>
2023-04-20 19:23:59 +0530

    removed redundant imports

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,14 +2,10 @@ import { LinksFunction } from '@remix-run/node';
 import styles from 'react-datepicker/dist/react-datepicker.css';
 import Calendar from '~/components/Calendar';
 import Navbar from '~/components/common/navbar';
-import { useStore } from '~/store/useStore';
-import { EventState, UserState } from '~/utils/interfaces';
 
 export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
 
 export default function Index() {
- 
-
   return (
     <div className="">
       <div className="flex flex-col-reverse md:flex-row ">
