7fe90f894b81ccb0b533a058e8d983a0c7804169
vivek lokhande <vivek2811999@gmail.com>
2023-05-01 21:23:32 +0530

    removed logs

--- app/mocks/browser.ts
+++ app/mocks/browser.ts
@@ -3,6 +3,5 @@ import { setupWorker } from 'msw';
 import { handlers } from './handlers/handlers';
 
 // This configures a Service Worker with the given request handlers.
-console.log({ handlers });
 
 export const worker = setupWorker(...handlers);
