2f9f18d2558aa505e04070bafdf04856ef4dd37f
vivek lokhande <vivek2811999@gmail.com>
2023-05-03 21:01:13 +0530

    changed handlers for both client and server in seperate files

--- app/mocks/handlers/serverHandler.ts
+++ app/mocks/handlers/serverHandler.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 const URL = process.env.API_HOST;
 
-export const handlers = [
+export const serverHandlers = [
   // Handles a POST /login request
   rest.get(`${URL}/users/usernameCheck/:username`, (req, res, ctx) => {
     // Persist user's authentication in the session
