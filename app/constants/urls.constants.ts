683f9e7b707dbcde8b6e947d1024860c1c1e2e3a
Rohit Gaur <knotjinx@gmail.com>
2023-05-11 11:28:13 -0400

    chore: rename URL to HOST

--- app/constants/urls.constants.ts
+++ app/constants/urls.constants.ts
@@ -1,5 +1,5 @@
-export const checkUsername = (URL: string, username: string) =>
-  `${URL}/users/usernameCheck/${username}`;
+export const checkUsername = (HOST: string, username: string) =>
+  `${HOST}/users/usernameCheck/${username}`;
 
 export const getEvents = (HOST: string, startTime: number, endTime: number) =>
   `${HOST}/events/calendar/${1}?startTime=${startTime}&endTime=${endTime}`;
