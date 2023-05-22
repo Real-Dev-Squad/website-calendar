fdfbea975bc35afe1b1fe51f37211be4ac025708
vivek lokhande <vivek2811999@gmail.com>
2023-05-22 09:53:43 +0530

    (#157) removed redundant functions and moved modal

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,20 +1,16 @@
-import axios from 'axios';
 import dayjs from 'dayjs';
 import { useState, useCallback, useEffect } from 'react';
 import { View } from 'react-big-calendar';
 import { CalendarEventProps, CalEvent } from '~/utils/interfaces';
-import EventModal from '~/components/common/eventModal';
 import RdsCalendar from '~/components/common/rdsCalendar';
 import { useStore } from '~/store/useStore';
-import { patchEvent } from '~/constants/urls.constants';
 
 interface CalendarProps {
   view?: View;
-  HOST: string;
 }
 
-const Calendar = ({ view, HOST }: CalendarProps) => {
-  const { events, updateEvent } = useStore((state) => state);
+const Calendar = ({ view }: CalendarProps) => {
+  const { events } = useStore((state) => state);
 
   const [eventsList, setEventsList] = useState<CalEvent[]>(events);
   const [calendarEvent, setCalendarEvent] = useState<CalendarEventProps>({
@@ -41,32 +37,6 @@ const Calendar = ({ view, HOST }: CalendarProps) => {
     );
   };
 
-  const updateEventStateFromModal = (event: CalEvent) => {
-    console.log(event);
-
-    setCalendarEvent((e) => ({ ...e, event }));
-    axios(patchEvent(HOST, event.id as number), {
-      method: 'patch',
-      data: { ...event, start: dayjs(event.start).valueOf(), end: dayjs(event.end).valueOf() },
-      withCredentials: true,
-    });
-    setEventsList((events) =>
-      events.map((e) => {
-        if (e.id === event.id) {
-          return event;
-        }
-        return e;
-      }),
-    );
-    updateEvent(event);
-  };
-
-  const addEvent = (event: CalEvent) => setEventsList((events) => [...events, event]);
-
-  const setShowEvent = (show: boolean) => {
-    setCalendarEvent((e) => ({ ...e, show }));
-  };
-
   const memoizedRdsCalendar = useCallback(
     () => (
       <RdsCalendar
@@ -83,18 +53,6 @@ const Calendar = ({ view, HOST }: CalendarProps) => {
   return (
     <>
       <div className="w-[100%]">{memoizedRdsCalendar()}</div>
-      {calendarEvent?.show && (
-        <EventModal
-          event={calendarEvent.event}
-          eventsList={eventsList}
-          currentEvent={calendarEvent?.event}
-          createEvent={addEvent}
-          updateEvent={updateEventStateFromModal}
-          setIsOpen={setShowEvent}
-          newEvent={calendarEvent.new}
-          setCalendarEvent={setCalendarEvent}
-        />
-      )}
     </>
   );
 };
