f4b10c90e322cfa33e69d1d9757799ed9d6bdb99
vivek lokhande <vivek2811999@gmail.com>
2023-05-22 09:54:03 +0530

    (#157) modal eventModal to a new route

--- app/components/common/eventModal/index.tsx
+++ app/components/common/eventModal/index.tsx
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { useTransition, useSubmit, Form, useNavigate } from '@remix-run/react';
 import * as Dialog from '@radix-ui/react-dialog';
 import dayjs from 'dayjs';
-import React, { useEffect, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import { CalendarEventProps, CalEvent } from '~/utils/interfaces';
 import UserInput from '../userInput';
@@ -14,7 +15,6 @@ interface EventModalProps {
   eventsList: CalEvent[];
   currentEvent: CalEvent | undefined;
   createEvent?: (event: CalEvent) => void;
-  updateEvent: (event: CalEvent) => void;
   isOpen?: boolean;
   setIsOpen: (show: boolean) => void;
   newEvent?: boolean;
@@ -25,20 +25,32 @@ export default function EventModal({
   event,
   eventsList,
   currentEvent,
-  createEvent,
-  updateEvent,
   isOpen = true,
-  setIsOpen,
-  newEvent = false,
   setCalendarEvent,
 }: EventModalProps) {
   const [eventDetail, setEventDetail] = useState<CalEvent | undefined>(event);
-
+  const submit = useSubmit();
+  const transition = useTransition();
+  const navigate = useNavigate();
   const minDate = dayjs(eventDetail?.start);
   const maxDate = dayjs(eventDetail?.end);
+  const text =
+    transition.state === 'submitting'
+      ? 'Saving...'
+      : transition.state === 'loading'
+      ? 'Saved!'
+      : 'submit';
 
   useEffect(() => {
-    setEventDetail({ ...eventDetail, title: event?.title, start: event?.start, end: event?.end });
+    setEventDetail({
+      ...eventDetail,
+      title: event?.title,
+      start: event?.start,
+      end: event?.end,
+      location: event?.location,
+      description: event?.description,
+      attendees: event?.attendees,
+    });
   }, [event]);
 
   const dateRange = (startDate: Date, endDate: Date) => {
@@ -51,133 +63,164 @@ export default function EventModal({
     return resultArray;
   };
 
+  const updateEventStateFromModal = (event: CalEvent) => {
+    setCalendarEvent((e) => ({ ...e, event }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(transition.state);
+    e.preventDefault();
+    // grab the form element
+    const $form = e.currentTarget;
+
+    // get the formData from that form
+    const formData = new FormData($form);
+
+    const payload = {
+      name: formData.get('title'),
+      startTime: dayjs(eventDetail?.start).valueOf(),
+      endTime: dayjs(eventDetail?.end).valueOf(),
+      location: formData.get('address'),
+      description: formData.get('description'),
+      attendees: eventDetail?.attendees
+        ? eventDetail.attendees.map(({ attendee }) => attendee.email)
+        : [],
+    };
+    const formDataPayload = new FormData();
+
+    Object.entries(payload).forEach(([key, value]) => {
+      formDataPayload.append(key as string, value as string);
+    });
+
+    submit(formDataPayload, {
+      method: 'patch',
+      action: $form.getAttribute('action') ?? $form.action,
+    });
+  };
+
   return (
     <Dialog.Root open={isOpen}>
       <Dialog.Portal>
         <Dialog.Overlay className="fixed inset-0 z-20 bg-black bg-opacity-40 animate-overlayShow duration-150 transition-timing-cubic-bezier-0.16-1-0.3-1" />
         <Dialog.Content className="z-30 fixed top-0 left-0 w-screen h-[95vh] my-6 box-border bg-white rounded-lg shadow-lg animate-contentShow duration-150 transition-timing-cubic-bezier-0.16-1-0.3-1 focus:outline-none">
           <div className="h-full w-full flex">
-            <div className="p-4 h-full w-full md:w-[400px] border-r-[1px] border-stone-50 overflow-auto">
-              <Dialog.Close asChild>
-                <button
-                  data-testid="modal-close-btn"
-                  className="rounded-lg outline-none cursor-pointer py-2 px-4 text-sm bg-neutral-100 border-neutral-200 border-[1px] text-neutral-500"
-                  aria-label="Close"
-                  onClick={() => setIsOpen(false)}
-                >
-                  CLOSE
-                </button>
-              </Dialog.Close>
+            <Form method="patch" onSubmit={handleSubmit}>
+              <div className="p-4 h-full w-full md:w-[400px] border-r-[1px] border-stone-50 overflow-auto">
+                <Dialog.Close asChild>
+                  <button
+                    data-testid="modal-close-btn"
+                    className="rounded-lg outline-none cursor-pointer py-2 px-4 text-sm bg-neutral-100 border-neutral-200 border-[1px] text-neutral-500"
+                    aria-label="Close"
+                    onClick={() => navigate(-1)}
+                  >
+                    CLOSE
+                  </button>
+                </Dialog.Close>
 
-              <div className="p-2">
-                <Dialog.Title>
-                  <UserInput
-                    dataTestId="modal-title"
-                    label=""
-                    placeholder="Enter Event Title"
-                    inputClassnames="border-none font-normal text-[32px] mb-4 text-stone-500 m-0 !bg-white"
-                    value={eventDetail?.title?.toString() ?? ''}
-                    setValue={(title) => setEventDetail((ev) => ({ ...ev, title }))}
-                  />
-                </Dialog.Title>
-
-                <EventVisibility
-                  visibility={eventDetail?.visibility ?? 'private'}
-                  setVisibility={(visibility) => setEventDetail((e) => ({ ...e, visibility }))}
-                />
-
-                <div className="mt-2">
-                  <div data-testid="modal-from-date">
-                    <p className="text-4 mb-2">From</p>
-                    <DatePicker
-                      placeholderText="from-date"
-                      className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full cursor-pointer"
-                      selected={minDate.toDate()}
-                      maxDate={maxDate.toDate()}
-                      timeIntervals={5}
-                      excludeTimes={dateRange(
-                        maxDate.toDate(),
-                        dayjs(maxDate).endOf('day').toDate(),
-                      )}
-                      onChange={(start) => {
-                        if (dayjs(start) < maxDate) {
-                          setEventDetail((e) => ({ ...e, start: dayjs(start).toDate() }));
-                        }
-                      }}
-                      showTimeSelect
-                      dateFormat="MMMM d, yyyy h:mm aa"
+                <div className="p-2">
+                  <Dialog.Title>
+                    <UserInput
+                      disabled={text !== 'submit'}
+                      dataTestId="modal-title"
+                      label=""
+                      name="title"
+                      placeholder="Enter Event Title"
+                      inputClassnames="border-none font-normal text-[32px] mb-4 text-stone-500 m-0 !bg-white"
+                      value={eventDetail?.title?.toString() ?? ''}
+                      setValue={(title) => setEventDetail((ev) => ({ ...ev, title }))}
                     />
-                  </div>
+                  </Dialog.Title>
 
-                  <div data-testid="modal-to-date">
-                    <p className="text-4 mb-2">To</p>
-                    <DatePicker
-                      placeholderText="to-date"
-                      className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full cursor-pointer"
-                      minDate={minDate.toDate()}
-                      selected={maxDate.toDate()}
-                      timeIntervals={5}
-                      excludeTimes={dateRange(
-                        dayjs(minDate).startOf('day').toDate(),
-                        minDate.toDate(),
-                      )}
-                      onChange={(end) => {
-                        if (minDate < dayjs(end)) {
-                          setEventDetail((e) => ({ ...e, end: dayjs(end).toDate() }));
-                        }
-                      }}
-                      showTimeSelect
-                      dateFormat="MMMM d, yyyy h:mm aa"
+                  <EventVisibility
+                    visibility={eventDetail?.visibility ?? 'private'}
+                    setVisibility={(visibility) => setEventDetail((e) => ({ ...e, visibility }))}
+                  />
+
+                  <div className="mt-2">
+                    <div data-testid="modal-from-date">
+                      <p className="text-4 mb-2">From</p>
+                      <DatePicker
+                        disabled={text !== 'submit'}
+                        placeholderText="from-date"
+                        className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full cursor-pointer"
+                        selected={minDate.toDate()}
+                        maxDate={maxDate.toDate()}
+                        timeIntervals={5}
+                        excludeTimes={dateRange(
+                          maxDate.toDate(),
+                          dayjs(maxDate).endOf('day').toDate(),
+                        )}
+                        onChange={(start) => {
+                          if (dayjs(start) < maxDate) {
+                            setEventDetail((e) => ({ ...e, start: dayjs(start).toDate() }));
+                          }
+                        }}
+                        showTimeSelect
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                      />
+                    </div>
+
+                    <div data-testid="modal-to-date">
+                      <p className="text-4 mb-2">To</p>
+                      <DatePicker
+                        disabled={text !== 'submit'}
+                        placeholderText="to-date"
+                        className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full cursor-pointer"
+                        minDate={minDate.toDate()}
+                        selected={maxDate.toDate()}
+                        timeIntervals={5}
+                        excludeTimes={dateRange(
+                          dayjs(minDate).startOf('day').toDate(),
+                          minDate.toDate(),
+                        )}
+                        onChange={(end) => {
+                          if (minDate < dayjs(end)) {
+                            setEventDetail((e) => ({ ...e, end: dayjs(end).toDate() }));
+                          }
+                        }}
+                        showTimeSelect
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                      />
+                    </div>
+
+                    <EmailChipsInput
+                      label="Attendees Emails"
+                      placeholder="Attendees Emails"
+                      attendees={eventDetail?.attendees ?? []}
+                      setAttendees={(attendees) => setEventDetail((ev) => ({ ...ev, attendees }))}
                     />
-                  </div>
 
-                  <EmailChipsInput
-                    label="Attendees Emails"
-                    placeholder="Attendees Emails"
-                    attendees={eventDetail?.attendees ?? []}
-                    setAttendees={(attendees) => setEventDetail((ev) => ({ ...ev, attendees }))}
-                  />
+                    <UserInput
+                      disabled={text !== 'submit'}
+                      dataTestId="modal-location"
+                      label="URL / Address"
+                      name="address"
+                      labelClassnames="text-4"
+                      placeholder="Enter URL or Address for the event"
+                      value={eventDetail?.location ?? ''}
+                      setValue={(location) => setEventDetail((ev) => ({ ...ev, location }))}
+                    />
 
-                  <UserInput
-                    dataTestId="modal-location"
-                    label="URL / Address"
-                    labelClassnames="text-4"
-                    placeholder="Enter URL or Address for the event"
-                    value={eventDetail?.location ?? ''}
-                    setValue={(location) => setEventDetail((ev) => ({ ...ev, location }))}
-                  />
-
-                  <p className="text-4 mb-2">Description</p>
-                  <textarea
-                    aria-label="Event Description"
-                    className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full"
-                    rows={2}
-                    cols={50}
-                    placeholder="Event Description"
-                    value={eventDetail?.description ?? ''}
-                    onChange={(e) =>
-                      setEventDetail((ev) => ({ ...ev, description: e.target.value }))
-                    }
-                  ></textarea>
-
-                  <Button
-                    dataTestId="modal-save-btn"
-                    label="Save"
-                    handleClick={() => {
-                      if (eventDetail) {
-                        if (createEvent && newEvent) {
-                          createEvent({ ...eventDetail, id: 4 });
-                        } else if (updateEvent) {
-                          updateEvent(eventDetail);
-                        }
+                    <p className="text-4 mb-2">Description</p>
+                    <textarea
+                      disabled={text !== 'submit'}
+                      aria-label="Event Description"
+                      className="bg-stone-50 text-4 p-3 mb-6 focus:outline-none border border-solid border-stone-400 rounded-lg w-full"
+                      rows={2}
+                      cols={50}
+                      name="description"
+                      placeholder="Event Description"
+                      value={eventDetail?.description ?? ''}
+                      onChange={(e) =>
+                        setEventDetail((ev) => ({ ...ev, description: e.target.value }))
                       }
-                      setIsOpen(false);
-                    }}
-                  />
+                    ></textarea>
+
+                    <Button dataTestId="modal-save-btn" label={text} type="submit" />
+                  </div>
                 </div>
               </div>
-            </div>
-
+            </Form>
             <div className="flex-auto hidden md:block">
               <RdsCalendar
                 height="100%"
@@ -186,7 +229,7 @@ export default function EventModal({
                 defaultDate={currentEvent?.start}
                 currentEvent={currentEvent}
                 setCalendarEvent={setCalendarEvent}
-                updateEvent={updateEvent}
+                updateEvent={updateEventStateFromModal}
               />
             </div>
           </div>
