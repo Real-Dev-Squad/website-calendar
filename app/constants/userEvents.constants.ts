26a7d5a7ff16892017823465c1c17e39ebbe5179
harshith-venkatesh <harshithece7016@gmail.com>
2023-06-30 16:00:37 +0530

    change the files to userEvents

---
+++ app/constants/userEvents.constants.ts
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+import { CalEvent } from "~/utils/interfaces";
+
+export const userEvents: CalEvent[] = [
+    
+    {
+      id: 1,
+      title: 'timed event',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel.',
+      location: 'Nashville, Tennessee. U.S.',
+      start: dayjs().add(1, 'hour').toDate(),
+      end: dayjs().add(2, 'hours').toDate(),
+      visibility: 'private',
+      attendees: [{ attendee: { email: 'a@b.c' } }, { attendee: { email: 'alpha@beta.gamma' } }],
+      meetLink: 'https://github.com/Real-Dev-Squad/website-calendar'
+    },
+  ];
