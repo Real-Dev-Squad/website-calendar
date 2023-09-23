c94eb535c6791b45c11e8630f1649cbcd5dfeb54
PeeyushPrashant <prashantpeeyush3003@gmail.com>
2023-09-23 20:36:10 +0530

    synced with develop branch

--- app/routes/socialEvents.tsx
+++ app/routes/socialEvents.tsx
@@ -9,7 +9,7 @@ import { CalEvent } from '~/utils/interfaces';
 export default function socialEvents() {
   const [selectedEvent, setSelectedEvent] = useState<CalEvent | null>(null);
   const [isDrawerVisible, setIsDrawerVisible] = useState(false);
-
+  
 
   const toggleDrawer = (event?: CalEvent) => {
     setSelectedEvent(event || null);
@@ -18,18 +18,18 @@ export default function socialEvents() {
 
   return <>
   <div className='flex'>
-        <Navbar />
+    <Navbar />
     <div className='flex flex-start flex-wrap gap-20 justify-center m-10'>
     {socialMockEvents.map(socialEvent => <div onClick={() => {
       toggleDrawer(socialEvent)}} className=""><SocialEventCard {...socialEvent} /></div>)}
-        </div>
-        {selectedEvent && (
+    </div>
+    {selectedEvent && (
           <Drawer
             event={selectedEvent}
             isDrawerVisible={isDrawerVisible}
             toggleDrawer={toggleDrawer}
           />
         )}
-      </div>
+  </div>
   </>;
 }
