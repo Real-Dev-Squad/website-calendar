367a77c868452734105c1cc28f7d232ba3f38fb1
Rohit Gaur <rohitgaur7@gmail.com>
2023-10-29 13:03:08 -0400

    feat-month-dropdown

---
+++ app/constants/months.constants.ts
@@ -0,0 +1,14 @@
+export const monthsArray = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
